593b541716cf68dd2f2c5005931d66b9d23b6b54
AdRuslan <russian.adig@mail.ru>
2021-07-10 18:04:14 +0500

    mern app completed

--- client/src/pages/Foreigner.js
+++ client/src/pages/Foreigner.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Question from '../components/Question';
 
 import './Foreigner.scss';
 
 export default function Foreigner() {
   const [foreigners, setForeigners] = useState([]);
-  const [expanded, setExpanded] = useState(false);
 
   const getAllForeigners = async () => {
     try {
@@ -29,28 +29,11 @@ export default function Foreigner() {
     <div className="container">
       <section className="info">
         {foreigners.map((item, index) => {
-          return (
-            <article className="question" key={index}>
-              <header className="question-header">
-                <h5
-                  onClick={() => setExpanded(!expanded)}
-                  className="question-title"
-                >
-                  {item.question}
-                </h5>
-                <button className="btn" onClick={() => setExpanded(!expanded)}>
-                  {expanded ? (
-                    <i className="material-icons prefix">arrow_upward</i>
-                  ) : (
-                    <i className="material-icons prefix">arrow_downward</i>
-                  )}
-                </button>
-              </header>
-              {expanded && <p>{item.answer}</p>}
-            </article>
-          );
+          return <Question key={index} {...item} />;
         })}
       </section>
     </div>
   );
 }
+
+//TODO: разделить аккордеон на компоненты как в примере на гитхабе
